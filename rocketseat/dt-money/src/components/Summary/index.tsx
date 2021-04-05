24c776b4051939a4d9005d0452c6f163597b8044
Johnatan Dantas <dantas.johnatan@gmail.com>
2021-04-04 21:06:06 -0300

    Adicionando o treinamento da rocketseat

---
+++ rocketseat/dt-money/src/components/Summary/index.tsx
@@ -0,0 +1,74 @@
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { useTransactions } from "../../hooks/useTransactions";
+
+import { Container } from "./styles";
+
+export function Summary() {
+  const {transactions} = useTransactions();
+
+  const summary = transactions.reduce((acc, transactions) => {
+    if (transactions.type === 'deposit') {
+      acc.deposits += transactions.amount;
+    }else {
+      acc.withdraws += transactions.amount;
+    }
+    acc.total = acc.deposits - acc.withdraws;
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas"/>
+        </header>
+        <strong>
+          {
+            new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.deposits)
+          }
+        </strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas"/>
+        </header>
+        <strong>
+          -
+          {
+            new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.withdraws)
+          }
+        </strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total"/>
+        </header>
+        <strong>
+          {
+            new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.total)
+          }
+        </strong>
+      </div>
+    </Container>
+  )
+}
